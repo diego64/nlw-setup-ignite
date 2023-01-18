849da6a749d25c7ad96d20b28342ebd989e7b714
diego64 <diegoferreira1964@gmail.com>
2023-01-18 20:33:55 -0300

    Base da estruta do server e front-end/mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import {
+import './src/lib/dayjs';
+
+import { StatusBar } from 'react-native';
+import { 
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home';
 
 import { Loading } from './src/components/Loading';
 
@@ -17,29 +21,16 @@ export default function App() {
     Inter_800ExtraBold
   });
 
-  if(!fontsLoaded){
-   (
-    <Loading />
-   )
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>APP NLW SETUP</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#7C3AED',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
