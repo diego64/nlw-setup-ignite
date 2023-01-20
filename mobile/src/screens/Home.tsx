4ac0b05442abb1fc5a9dfb93e10d4f2148a76293
diego64 <diegoferreira1964@gmail.com>
2023-01-19 21:41:50 -0300

    Finalizando o back-end e a estrutura do front-end/mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { Text, View, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { generateRangeDatesFromYearStart } from '../utils/generate-dates-from-year-beginning';
 
@@ -12,6 +13,8 @@ const minimunSummaryDatesSizes = 18 * 3;
 const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
@@ -37,7 +40,10 @@ export function Home() {
         <View className='flex-row flex-wrap'>
           {
             datesFromYearStart.map(date => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => { navigate('habit', { date: date.toISOString() })}}
+            />
             ))
           }
 
