38bcf9e9d7c2373e0ed191240941f2fba946e047
diego64 <diegoferreira1964@gmail.com>
2023-01-22 15:25:44 -0300

    Corrigindo o arquivo

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,22 +1,49 @@
+import { useState, useEffect } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { ScrollView, Text, View } from "react-native";
 import dayjs from "dayjs";
+
+import { api } from "../lib/axios";
 
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/Progress.Bar";
 import { Checkbox } from "../components/Checkbox";
+import { Loading } from "../components/Loading";
 
 interface Params {
   date: string;
 }
 
 export function Habit() {
+  const [loading, setLoading] = useState(true);
   const route = useRoute()
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
   const dayOfWeek = parsedDate.format('dddd');
   const dayAndMonth = parsedDate.format('DD/MM');
+
+  async function fatchHabits() {
+    try {
+      setLoading(true);
+
+      const response = await api.get('/day', { params: { date }});
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Ops', 'Não foi possível carregar as informações dos hábitos')
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fatchHabits();
+  }, []);
+
+  if(loading) {
+    <Loading />
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
